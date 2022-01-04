06c30ba96e1efe0702fdf8710d12bacc8055a0ca
Ульви Рзазаде <ulva95@gmail.com>
2022-01-05 00:29:20 +0300

    fix

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -12,6 +12,8 @@ scp -r ../otl-client rathet@${DEPLOY_SERVER}:~/apps/${SERVER_FOLDER}/
 echo "Finished copying the build files"
 ssh rathet@${DEPLOY_SERVER} 'cd ~/apps/otlivchik/otl-client'
 ssh rathet@${DEPLOY_SERVER} 'pwd'
+ssh rathet@${DEPLOY_SERVER} 'docker stop $(docker ps -q --filter ancestor=rathet/otlc:latest )'
+ssh rathet@${DEPLOY_SERVER} 'docker rmi --force $(docker images -q "rathet/otlc" | uniq)'
 ssh rathet@${DEPLOY_SERVER} 'docker build --no-cache -t rathet/otlc ~/apps/otlivchik/otl-client'
 echo "Finished building docker"
 ssh rathet@${DEPLOY_SERVER} 'docker run -d -t -p 3001:3001  rathet/otlc:latest'
